be5c275a9f67ff2fb5432c9323401df27f442a84
Fabianno Limma <fabiannolimma20287@gmail.com>
2023-08-21 07:57:46 -0300

    novas atualizações

--- setting/configuracoes.php
+++ setting/configuracoes.php
@@ -1,13 +1,12 @@
 <?php
+
 require_once('database.php');
 //pegando dados do site
-//////////////////////////////////////////////
-function DataBase($user, $pass, $host, $db){
-//corpo da função    
-}                               
+//////////////////////////////////////////////                        
 ///////////////////////////////////////////////
 $nomeSite = "SistemasFL";
 $descSite = "Desenvolvimento de sistemas web para pequenas empresas";
 $tagsSite = "empresas, gestão, gerenciador, marmoraria";
 $emailSite = "contato@sistemafl.com";
+
 ?>
